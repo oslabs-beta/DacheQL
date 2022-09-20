8efe2ffcb1a3bc8cadddf8024c01e4262e6adc9f
Ethan <ethanchuang04@gmail.com>
2022-09-20 13:49:21 -0400

    AWS RDS integration completed

---
+++ server/models/models.js
@@ -0,0 +1,25 @@
+const { Pool, Client } = require("pg");
+
+const pool = new Pool({
+  host: "database-1.cvaifkhdgcwh.us-east-1.rds.amazonaws.com",
+  port: 5432,
+  user: "dacheql",
+  database: "dacheql",
+  password: "addy19compsci",
+});
+
+pool.connect((err) => {
+  if (err) {
+    console.log("error: ", err);
+    return err;
+  }
+
+  console.log("it worked");
+});
+
+module.exports = {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback);
+  },
+};
+
