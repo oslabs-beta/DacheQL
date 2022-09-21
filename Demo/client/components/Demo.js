254a76d236aeacb9f16e74151164b4d70bb33ce7
Andrew Moy <ajmoy35@gmail.com.com>
2022-09-21 18:51:47 -0400

    demo page beginning

--- Demo/client/components/Demo.js
+++ Demo/client/components/Demo.js
@@ -1,10 +1,74 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Navigation from './Navigation';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { DropdownButton, Dropdown, Button, Card, Container } from 'react-bootstrap';
+import Query from './Query';
+import Metrics from './Metrics';
+
+
 const Demo = () => {
+  const [query, setQuery] = useState('Select Query');
+  const [output, setOutput] = useState('');
+  const [queryString, setQueryString] = useState('');
+
+  const handleChangeValorant = (event) => {
+    console.log(event.target.innerHTML);
+    setQuery(event.target.innerHTML);
+    setOutput('Query For Valorant');
+  };
+
+  const handleChangePokemon = (event) => {
+    console.log(event.target.innerHTML);
+    setQuery(event.target.innerHTML);
+    setOutput('Query For Pokemon');
+  };
+
+  const handleChangeCities = (event) => {
+    console.log(event.target.innerHTML);
+    setQuery(event.target.innerHTML);
+    setOutput('Query For Cities');
+  };
+
+  const runQuery = () =>{
+    console.log('running query!');
+  };
+
   return (
     <div>
       <Navigation></Navigation>
-			This is the Demo Page
+      <Button onClick = {runQuery}>Run Query</Button>
+      <Card style={{color: '#000', width: '20rem', height: '20rem'}}>
+        <Card.Body>
+          <Card.Title>
+            Selected Query:
+          </Card.Title>
+          <Card.Text>
+            <Query output = {output} />
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Dropdown >
+        <Dropdown.Toggle variant = "secondary" id ="query-dropdown">
+          {query}
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href = "#/action-1" onClick = {handleChangeValorant}>Query For Valorant</Dropdown.Item>
+          <Dropdown.Item href = "#/action-2" onClick = {handleChangePokemon}>Query For Pokemon</Dropdown.Item>
+          <Dropdown.Item href = "#/action-3" onClick = {handleChangeCities}>Query For Cities</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
+      <Card style={{color: '#000', width: '20rem', height: '20rem'}}>
+        <Card.Body>
+          <Card.Title>
+            Metrics
+          </Card.Title>
+          <Card.Text>
+            <Metrics />
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </div>
   ); 
 };
--- Demo/server/schema/cityData.js
+++ Demo/server/schema/cityData.js
@@ -1,184 +1,184 @@
 const cities = [
-    {
-        "id": "2",
-        "name": "El Tarter",
-        "population": 1052,
-        "country_id": "1"
-      },
-      {
-        "id": "3",
-        "name": "La Massana",
-        "population": 7211,
-        "country_id": "1"
-      },
-      {
-        "id": "4",
-        "name": "Canillo",
-        "population": 3292,
-        "country_id": "1"
-      },
-      {
-        "id": "5",
-        "name": "Andorra la Vella",
-        "population": 20430,
-        "country_id": "1"
-      },
-      {
-        "id": "6",
-        "name": "Jorochito",
-        "population": 4013,
-        "country_id": "2"
-      },
-      {
-        "id": "7",
-        "name": "Tupiza",
-        "population": 22233,
-        "country_id": "2"
-      },
-      {
-        "id": "8",
-        "name": "Puearto Pailas",
-        "population": 0,
-        "country_id": "2"
-      },
-      {
-        "id": "9",
-        "name": "Capinota",
-        "population": 5157,
-        "country_id": "2"
-      },
-      {
-        "id": "10",
-        "name": "Camargo",
-        "population": 4715,
-        "country_id": "2"
-      },
-      {
-        "id": "11",
-        "name": "Villa Serrano",
-        "population": 0,
-        "country_id": "2"
-      },
-      {
-        "id": "12",
-        "name": "Voskevaz",
-        "population": 3789,
-        "country_id": "3"
-      },
-      {
-        "id": "13",
-        "name": "Gavarr",
-        "population": 21680,
-        "country_id": "3"
-      },
-      {
-        "id": "14",
-        "name": "Nizami",
-        "population": 1060,
-        "country_id": "3"
-      },
-      {
-        "id": "15",
-        "name": "Metsavan",
-        "population": 4767,
-        "country_id": "3"
-      },
-      {
-        "id": "16",
-        "name": "Hnaberd",
-        "population": 1817,
-        "country_id": "3"
-      },
-      {
-        "id": "17",
-        "name": "Tafuna",
-        "population": 11017,
-        "country_id": "4"
-      },
-      {
-        "id": "18",
-        "name": "Aua",
-        "population": 2124,
-        "country_id": "4"
-      },
-      {
-        "id": "19",
-        "name": "Malaeimi",
-        "population": 1261,
-        "country_id": "4"
-      },
-      {
-        "id": "20",
-        "name": "Taulaga",
-        "population": 37,
-        "country_id": "4"
-      },
-      {
-        "id": "21",
-        "name": "Fagatogo",
-        "population": 1868,
-        "country_id": "4"
-      },
-      {
-        "id": "22",
-        "name": "Oranjestad",
-        "population": 29998,
-        "country_id": "5"
-      },
-      {
-        "id": "23",
-        "name": "Paradera",
-        "population": 12024,
-        "country_id": "5"
-      },
-      {
-        "id": "24",
-        "name": "Seoul",
-        "population": 9733509,
-        "country_id": "6"
-      },
-      {
-        "id": "25",
-        "name": "Uiwang",
-        "population": 158482,
-        "country_id": "6"
-      },
-      {
-        "id": "26",
-        "name": "Incheon",
-        "population": 2801039,
-        "country_id": "6"
-      },
-      {
-        "id": "27",
-        "name": "Tokyo",
-        "population": 13960236,
-        "country_id": "7"
-      },
-      {
-        "id": "28",
-        "name": "Osaka",
-        "population": 19165340,
-        "country_id": "7"
-      },
-      {
-        "id": "29",
-        "name": "Okinawa",
-        "population": 1384762,
-        "country_id": "7"
-      },
-      {
-        "id": "30",
-        "name": "George Town",
-        "population": 40200,
-        "country_id": "8"
-      },
-      {
-        "id": "31",
-        "name": "West Bay",
-        "population": 15436,
-        "country_id": "8"
-      }
+  {
+    'id': '2',
+    'name': 'El Tarter',
+    'population': 1052,
+    'country_id': '1'
+  },
+  {
+    'id': '3',
+    'name': 'La Massana',
+    'population': 7211,
+    'country_id': '1'
+  },
+  {
+    'id': '4',
+    'name': 'Canillo',
+    'population': 3292,
+    'country_id': '1'
+  },
+  {
+    'id': '5',
+    'name': 'Andorra la Vella',
+    'population': 20430,
+    'country_id': '1'
+  },
+  {
+    'id': '6',
+    'name': 'Jorochito',
+    'population': 4013,
+    'country_id': '2'
+  },
+  {
+    'id': '7',
+    'name': 'Tupiza',
+    'population': 22233,
+    'country_id': '2'
+  },
+  {
+    'id': '8',
+    'name': 'Puearto Pailas',
+    'population': 0,
+    'country_id': '2'
+  },
+  {
+    'id': '9',
+    'name': 'Capinota',
+    'population': 5157,
+    'country_id': '2'
+  },
+  {
+    'id': '10',
+    'name': 'Camargo',
+    'population': 4715,
+    'country_id': '2'
+  },
+  {
+    'id': '11',
+    'name': 'Villa Serrano',
+    'population': 0,
+    'country_id': '2'
+  },
+  {
+    'id': '12',
+    'name': 'Voskevaz',
+    'population': 3789,
+    'country_id': '3'
+  },
+  {
+    'id': '13',
+    'name': 'Gavarr',
+    'population': 21680,
+    'country_id': '3'
+  },
+  {
+    'id': '14',
+    'name': 'Nizami',
+    'population': 1060,
+    'country_id': '3'
+  },
+  {
+    'id': '15',
+    'name': 'Metsavan',
+    'population': 4767,
+    'country_id': '3'
+  },
+  {
+    'id': '16',
+    'name': 'Hnaberd',
+    'population': 1817,
+    'country_id': '3'
+  },
+  {
+    'id': '17',
+    'name': 'Tafuna',
+    'population': 11017,
+    'country_id': '4'
+  },
+  {
+    'id': '18',
+    'name': 'Aua',
+    'population': 2124,
+    'country_id': '4'
+  },
+  {
+    'id': '19',
+    'name': 'Malaeimi',
+    'population': 1261,
+    'country_id': '4'
+  },
+  {
+    'id': '20',
+    'name': 'Taulaga',
+    'population': 37,
+    'country_id': '4'
+  },
+  {
+    'id': '21',
+    'name': 'Fagatogo',
+    'population': 1868,
+    'country_id': '4'
+  },
+  {
+    'id': '22',
+    'name': 'Oranjestad',
+    'population': 29998,
+    'country_id': '5'
+  },
+  {
+    'id': '23',
+    'name': 'Paradera',
+    'population': 12024,
+    'country_id': '5'
+  },
+  {
+    'id': '24',
+    'name': 'Seoul',
+    'population': 9733509,
+    'country_id': '6'
+  },
+  {
+    'id': '25',
+    'name': 'Uiwang',
+    'population': 158482,
+    'country_id': '6'
+  },
+  {
+    'id': '26',
+    'name': 'Incheon',
+    'population': 2801039,
+    'country_id': '6'
+  },
+  {
+    'id': '27',
+    'name': 'Tokyo',
+    'population': 13960236,
+    'country_id': '7'
+  },
+  {
+    'id': '28',
+    'name': 'Osaka',
+    'population': 19165340,
+    'country_id': '7'
+  },
+  {
+    'id': '29',
+    'name': 'Okinawa',
+    'population': 1384762,
+    'country_id': '7'
+  },
+  {
+    'id': '30',
+    'name': 'George Town',
+    'population': 40200,
+    'country_id': '8'
+  },
+  {
+    'id': '31',
+    'name': 'West Bay',
+    'population': 15436,
+    'country_id': '8'
+  }
 ]
 
 module.exports = cities;
