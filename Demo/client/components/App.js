1bb2aed4d7a7ea640d894fb685dea0b9c836dd3e
chunhz <chunhz062009@gmail.com>
2022-09-27 12:18:05 -0400

    sticky navbar on top

--- Demo/client/components/App.js
+++ Demo/client/components/App.js
@@ -15,14 +15,11 @@ import Team from './Team.js';
 const App = () => {
   // const logo = document.querySelectorAll('#logo path');
   // console.log(logo);
-  window.onscroll = function() {myFunction();};
 
-  var navbar = document.getElementsByClassName('navbar');
-  var sticky = navbar.offsetTop;
   document.addEventListener('DOMContentLoaded', function(){
     window.addEventListener('scroll', function() {
       if (window.scrollY > 50) {
-        document.getElementById('navbar_top').classList.add('fixed-top');
+        document.getElementById('navbar').classList.add('fixed-top');
         // add padding top to show content behind navbar
         const navbar_height = document.querySelector('.navbar').offsetHeight;
         document.body.style.paddingTop = navbar_height + 'px';
@@ -33,15 +30,7 @@ const App = () => {
       } 
     });
   }); 
-  function myFunction() {
-    console.log('scrolling');
-    console.log('sticky', window.pageYOffset);
-    // if (window.pageYOffset >= sticky) {
-    //   navbar.classList.add('sticky');
-    // } else {
-    //   navbar.classList.remove('sticky');
-    // }
-  }
+
   return (
     <BrowserRouter>
       <Routes>
