eab66990d80c4c235ce4114c87a737dcbd6eeb82
Andrew Moy <ajmoy35@gmail.com.com>
2022-10-05 16:33:37 -0400

    working on docs page

--- Demo/client/components/Docs.js
+++ Demo/client/components/Docs.js
@@ -1,31 +1,22 @@
-import {React, useState} from 'react';
+import React from 'react';
 import Navigation from './Navigation';
+import {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { withRouter } from 'react-router';
-// import './styles/aboutStyles.css';
+import './styles/aboutStyles.css';
 import Sidebar from './Sidebar';
 import { DropdownButton, Dropdown, Button, Card, Container, Row, Col, Form, FormLabel } from 'react-bootstrap';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 
 const Docs = (props) => {
-  // const [value, setValue] = useState('');
-  // const [copied, setCopied] = useState(false);
+  const [value, setValue] = useState('');
+  const [copied, setCopied] = useState(false);
   
   return(
     <div>
-      <Navigation id='navbar'></Navigation> */
-      {/* <div>
-        <Row>
-          <Col>
-            <Card style={{ color: '#000', width: '30rem', height: '30rem', right: '10px' }} className='selected-query'>
-              <Card.Body>
-              
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </div> */}
+      <Navigation id='navbar'></Navigation>
+    
       <p>HIIIII</p>
     </div>
   );
--- Demo/client/components/Navigation.js
+++ Demo/client/components/Navigation.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { Navbar, Nav} from 'react-bootstrap';
 import './styles/navbarStyles.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faLaptopCode, faAddressCard, faDownload} from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faLaptopCode, faAddressCard, faDownload, faFolderOpen} from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faNpm } from '@fortawesome/free-brands-svg-icons';
 import templogo from './assets/templogo.png';
 
@@ -22,6 +22,7 @@ const Navigation = () => {
       <Navbar.Collapse id='navbar-nav'>
         <Nav className="nav-items" navbarScroll style={{ justifyContent: 'center', right: '5vw',  position:'absolute'  }}>
           <Nav.Link className='nav-item' as={Link} to='/homepage' ><FontAwesomeIcon icon={faHouse}></FontAwesomeIcon >&nbsp;Home</Nav.Link>
+          <Nav.Link className='nav-item' as={Link} to='/docs' ><FontAwesomeIcon icon={faFolderOpen}></FontAwesomeIcon >&nbsp;Docs</Nav.Link>
           <Nav.Link className='nav-item' as={Link} to='/demo'><FontAwesomeIcon icon={faLaptopCode} />&nbsp;Demo</Nav.Link>
           <Nav.Link className='nav-item' as={Link} to='/about'><FontAwesomeIcon icon={faAddressCard} />&nbsp;About Us</Nav.Link>
           <Nav.Link className='nav-item' href='https://github.com/oslabs-beta/DacheQL' target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} />&nbsp;GitHub</Nav.Link>
