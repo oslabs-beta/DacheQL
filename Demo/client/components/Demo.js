01666cdad56ff4e8ace401bb715d0b054bbb9546
Ethan <ethanchuang04@gmail.com>
2022-10-10 12:23:29 -0400

    done with cleanup

--- Demo/client/components/Demo.js
+++ Demo/client/components/Demo.js
@@ -323,10 +323,10 @@ const Demo = () => {
       <div className='howto'>
         <h1>How To Use</h1>
         <ol> 
-          <li>Select a query you want to receive information for</li>
-          <li>Press Run Query and see the uncached fetch time</li>
+          <li>Select a query you want to receive information for.</li>
+          <li>Press Run Query and see the uncached fetch time.</li>
           <li>Press Run Query a second time and notice the cached runtime!</li>
-          <p> * Repeated query requests to cached data will remain around similar runtime! *</p>
+          <p> <br></br> * It should be noted the first ever query will have a significantly higher runtime than the other first queries because it is establishing a connection.<br></br> In addition, if you return to a previous query you have already ran, the uncached runtime time on the graph will show as zero as it is already stored in the cache. *</p>
         </ol>
       </div>
       <div className='card-container'>
