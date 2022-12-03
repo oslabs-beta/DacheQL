a8af69b697b3567dd4f50baabc3c634e2ebf5ea4
ChunHao Zheng <46112578+chunhz@users.noreply.github.com>
2022-12-02 21:39:59 -0500

    Update server.ts

--- Demo/server/server.ts
+++ Demo/server/server.ts
@@ -9,7 +9,7 @@ const redis = require('redis');
 // const dacheQL = require('dacheql');
 const dacheQL = require('./controllers/redisController.js')
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // const REDIS_PORT = process.env.PORT || 6379;
 // const client = redis.createClient(REDIS_PORT);
